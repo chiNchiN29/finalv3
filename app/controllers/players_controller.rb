474bd8f910e0c77f89f8c47a3285350fc35ae388
Jiah Margallo <jiahmargallo@gmail.com>
2014-05-22 14:52:12 +0800

    added round

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,9 +2,18 @@ class PlayersController < ApplicationController
   def index
 	@player = Player.new
   end
-
+  def play
+  	@player = Player.new
+  end
   def round
-  	@players = Player.new
+  	@player = Player.new
+  	@player.p1_name = 
+  	@player.p2_name = 
+  	@player.p1_bet = 
+  	@player.p2_bet = 
+  	@player.p2_money = 
+  	@player.p2_money = 
+  	@player.save!
   end
 
   def result
