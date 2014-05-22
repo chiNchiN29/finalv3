8d5ad9cf5560e509db18de94579a6a6572b91cec
Jiah Margallo <jiahmargallo@gmail.com>
2014-05-22 15:33:09 +0800

    edit new

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,9 +3,15 @@ class PlayersController < ApplicationController
   end
 
   def create
+  	@player = Player.new
+  	@player.p1_bet = params[:player][:p1_bet]
+  	@player.p2_bet = params[:player][:p2_bet]
+  	@player.save!
+  	redirect_to :action => :index
   end
 
   def new
+  	@player = Player.new
   end
 
   def show
