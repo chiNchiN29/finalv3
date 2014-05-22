8224d4c40e35ae0d80c5610deda99c8b71850988
Jiah Margallo <jiahmargallo@gmail.com>
2014-05-22 15:01:06 +0800

    edit round

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,24 +2,29 @@ class PlayersController < ApplicationController
   def index
 	@player = Player.new
   end
+
   def play
   	@player = Player.new
 	@player.p1_name = params[:player][:p1_name]
 	@player.p2_name = params[:player][:p2_name]
 	@player.save!
   end
+  
   def round
   	@player = Player.new
-  	@player.p1_name = 
-  	@player.p2_name = 
-  	@player.p1_bet = 
-  	@player.p2_bet = 
-  	@player.p2_money = 
-  	@player.p2_money = 
+  	#@player.p1_name = 
+  	#@player.p2_name = 
+
+  	@player.p1_bet = params[:player][:p1_bet1]
+  	@player.p2_bet = params[:player][:p1_bet1]
+  	#@player.p2_money = 
+  	#@player.p2_money = 
   	@player.save!
+  	redirect_to :action => :result
   end
 
   def result
+  	@player = Player.find(params[:id])
   end
 
   def summary
