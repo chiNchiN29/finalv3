cc9b5037fe2e21ba9225a2ca7f260290ce9409af
chiNchiN29 <chinchin_chua29@gmail.com>
2014-05-24 00:48:27 +0800

    images with values. shows up index.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,12 @@
 class PlayersController < ApplicationController
   def index
 	@players = Player.all	
+	session[:deck] = []
+	suit  = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
+	values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
+	session[:deck] = suit.product(values).shuffle!
+	@cards = []
+	@cards = session[:deck].sample(3)
   end
 
   def create
--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,5 @@
 module PlayersHelper
+	def display_image(card)
+		image_tag("#{card[0]}_#{card[1]}.jpg")
+	end
 end
